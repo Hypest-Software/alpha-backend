f91b9bb96ff2fc852bbc19179f9fdd78caae417f
BartlomiejRasztabiga <contact@rasztabiga.me>
2020-11-15 22:48:52 +0100

    Run app in dev profile by default

--- src/test/kotlin/dev/hypestsoftware/hackyeah2020/backend/ApplicationTests.kt
+++ src/test/kotlin/dev/hypestsoftware/hackyeah2020/backend/ApplicationTests.kt
@@ -2,8 +2,10 @@ package dev.hypestsoftware.hackyeah2020.backend
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("development")
 class ApplicationTests {
 
 	@Test
