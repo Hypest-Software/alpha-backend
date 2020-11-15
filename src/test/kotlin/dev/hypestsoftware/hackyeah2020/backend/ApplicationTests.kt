4b7d6f453b33e4470a650094d94900c2357c85e0
BartlomiejRasztabiga <contact@rasztabiga.me>
2020-11-15 21:46:35 +0100

    Init project with spring initializr

---
+++ src/test/kotlin/dev/hypestsoftware/hackyeah2020/backend/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.hypestsoftware.hackyeah2020.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
