c50ae8351c280b68389e2c42aebedf413b58e424
BartlomiejRasztabiga <contact@rasztabiga.me>
2020-11-15 23:31:25 +0100

    Reformat code

--- src/test/kotlin/dev/hypestsoftware/hackyeah2020/backend/ApplicationTests.kt
+++ src/test/kotlin/dev/hypestsoftware/hackyeah2020/backend/ApplicationTests.kt
@@ -8,8 +8,8 @@ import org.springframework.test.context.ActiveProfiles
 @ActiveProfiles("development")
 class ApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+    }
 
 }
