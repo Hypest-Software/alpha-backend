0862b8d63e3c976e5f7f3e4bb1c50fc93701bb39
BartlomiejRasztabiga <contact@rasztabiga.me>
2020-11-16 23:26:38 +0100

    Replace magic string with property value

--- src/main/kotlin/dev/hypestsoftware/hackyeah2020/backend/config/CustomTokenConverter.kt
+++ src/main/kotlin/dev/hypestsoftware/hackyeah2020/backend/config/CustomTokenConverter.kt
@@ -2,6 +2,7 @@ package dev.hypestsoftware.hackyeah2020.backend.config
 
 import dev.hypestsoftware.hackyeah2020.backend.security.UserPrincipal
 import dev.hypestsoftware.hackyeah2020.backend.service.UserService
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken
 import org.springframework.security.oauth2.common.OAuth2AccessToken
 import org.springframework.security.oauth2.provider.OAuth2Authentication
@@ -10,10 +11,13 @@ import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenCo
 class CustomTokenConverter(private val userService: UserService) :
     JwtAccessTokenConverter() {
 
+    @Value("\${app.oauth.webapp.client-id}")
+    private lateinit var webappClientId: String
+
     override fun enhance(accessToken: OAuth2AccessToken?, authentication: OAuth2Authentication?): OAuth2AccessToken? {
         val additionalInfo = HashMap<String, Any>()
 
-        if (authentication!!.oAuth2Request.clientId == "webapp") {
+        if (authentication!!.oAuth2Request.clientId == webappClientId) {
             val principal = authentication.principal as UserPrincipal
             val user = userService.getUserByEmail(principal.username)
 
