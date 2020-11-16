188c841a71baf6a93679bcbe5493e18b829f228a
BartlomiejRasztabiga <contact@rasztabiga.me>
2020-11-16 12:00:10 +0100

    Close User and Role classes

--- src/main/kotlin/dev/hypestsoftware/hackyeah2020/backend/model/Role.kt
+++ src/main/kotlin/dev/hypestsoftware/hackyeah2020/backend/model/Role.kt
@@ -3,9 +3,7 @@ package dev.hypestsoftware.hackyeah2020.backend.model
 import java.util.UUID
 import javax.persistence.Column
 import javax.persistence.Entity
-import javax.persistence.EnumType
 import javax.persistence.Enumerated
-import javax.persistence.FetchType
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
 import javax.persistence.ManyToMany
@@ -13,15 +11,15 @@ import javax.persistence.Table
 
 @Entity
 @Table(name = "roles")
-open class Role(
+class Role(
     @Id
     @GeneratedValue
     @Column(columnDefinition = "uuid", updatable = false)
-    open val uuid: UUID = UUID.randomUUID(),
+    val uuid: UUID = UUID.randomUUID(),
 
     @Enumerated(EnumType.STRING)
-    open var name: RoleName,
+    var name: RoleName,
 
     @ManyToMany(fetch = FetchType.EAGER, mappedBy = "roles")
-    open var users: MutableSet<User> = mutableSetOf()
+    var users: MutableSet<User> = mutableSetOf()
 )
