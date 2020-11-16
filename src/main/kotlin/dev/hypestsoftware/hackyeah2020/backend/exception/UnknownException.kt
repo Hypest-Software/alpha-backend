60915ab23e7d6633b91849cb10e10856be90da79
BartlomiejRasztabiga <contact@rasztabiga.me>
2020-11-16 23:47:19 +0100

    Delete unused apiErrorCode param in UnknownException.kt

--- src/main/kotlin/dev/hypestsoftware/hackyeah2020/backend/exception/UnknownException.kt
+++ src/main/kotlin/dev/hypestsoftware/hackyeah2020/backend/exception/UnknownException.kt
@@ -2,4 +2,4 @@ package dev.hypestsoftware.hackyeah2020.backend.exception
 
 import org.springframework.http.HttpStatus
 
-class UnknownException(apiErrorCode: ApiErrorCode) : RestException("Unknown error", HttpStatus.INTERNAL_SERVER_ERROR)
+class UnknownException : RestException("Unknown error", HttpStatus.INTERNAL_SERVER_ERROR)
