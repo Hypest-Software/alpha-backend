c2a74fc2b648766d82c91e3bcdda8e854d81391e
BartlomiejRasztabiga <contact@rasztabiga.me>
2020-11-17 11:14:41 +0100

    Return ResponseEntity instead of plain object

--- src/main/kotlin/dev/hypestsoftware/hackyeah2020/backend/controller/UserController.kt
+++ src/main/kotlin/dev/hypestsoftware/hackyeah2020/backend/controller/UserController.kt
@@ -7,6 +7,7 @@ import dev.hypestsoftware.hackyeah2020.backend.service.UserService
 import dev.hypestsoftware.hackyeah2020.backend.utils.PUBLIC_API_ENDPOINT_V1
 import dev.hypestsoftware.hackyeah2020.backend.utils.TokenInfo
 import dev.hypestsoftware.hackyeah2020.backend.utils.ValidationUtils
+import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -20,7 +21,7 @@ class UserController(
 ) {
     @GetMapping("/me")
     @PreAuthorize("hasAnyAuthority('ROLE_ADMIN', 'ROLE_USER')")
-    fun getMe(): CurrentUserDto {
+    fun getMe(): ResponseEntity<CurrentUserDto> {
         val uuid = tokenInfo.userUUID ?: ApiErrorCode.AUTH_0003.throwException()
         if (!ValidationUtils.isStringUUID(uuid)) {
             ApiErrorCode.AUTH_0003.throwException()
@@ -28,10 +29,12 @@ class UserController(
 
         val user: User = userService.getUserByUUID(uuid) ?: ApiErrorCode.AUTH_0004.throwException()
 
-        return CurrentUserDto(
-            username = user.username,
-            roles = user.roles.map { it.name.name }.toSet(),
-            userUUID = user.uuid.toString()
+        return ResponseEntity.ok(
+            CurrentUserDto(
+                username = user.username,
+                roles = user.roles.map { it.name.name }.toSet(),
+                userUUID = user.uuid.toString()
+            )
         )
     }
 }
