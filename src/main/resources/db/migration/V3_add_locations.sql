fab90c5458da319054e8fbac81f162f46d4f86ca
Tomasz Domagała <tomekdo3@gmail.com>
2020-11-27 20:03:43 +0100

    create locations database table

---
+++ src/main/resources/db/migration/V3_add_locations.sql
@@ -0,0 +1,9 @@
+CREATE TABLE locations
+(
+    id        integer PRIMARY KEY,
+    latitude  numeric(24, 16) NOT NULL,
+    longitude numeric(24, 16) NOT NULL
+);
+
+CREATE INDEX latitude_index ON locations (latitude);
+CREATE INDEX longitude_index ON locations (longitude);
