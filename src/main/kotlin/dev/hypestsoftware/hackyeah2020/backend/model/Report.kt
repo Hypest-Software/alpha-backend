160a4967320f06d95d97906ef3434424cc392f24
Tomasz Domagała <tomekdo3@gmail.com>
2020-11-27 19:20:35 +0100

    create kotlin class model of reports

---
+++ src/main/kotlin/dev/hypestsoftware/hackyeah2020/backend/model/Report.kt
@@ -0,0 +1,26 @@
+package dev.hypestsoftware.hackyeah2020.backend.model
+
+import org.springframework.data.annotation.CreatedDate
+import java.sql.Timestamp
+import java.util.*
+import javax.persistence.*
+
+@Entity
+@Table(name = "reports")
+class Report(
+        @Id
+        @GeneratedValue
+        @Column(columnDefinition = "uuid", updatable = false)
+        val uuid: UUID = UUID.randomUUID(),
+
+        @CreatedDate
+        @Column(nullable = false)
+        val createdAt: Timestamp,
+
+        @Column(nullable = false)
+        val description: String,
+
+        @Column(nullable = false)
+        val imageUrl: String,
+
+        )
