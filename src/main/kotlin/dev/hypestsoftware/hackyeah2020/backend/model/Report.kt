9296dc61b85e595e9be722dc91ab3142f4814e24
Tomasz Domagała <tomekdo3@gmail.com>
2020-11-27 21:22:32 +0100

    fix formatting for this fucking linter

--- src/main/kotlin/dev/hypestsoftware/hackyeah2020/backend/model/Report.kt
+++ src/main/kotlin/dev/hypestsoftware/hackyeah2020/backend/model/Report.kt
@@ -2,30 +2,35 @@ package dev.hypestsoftware.hackyeah2020.backend.model
 
 import org.springframework.data.annotation.CreatedDate
 import java.sql.Timestamp
-import java.util.*
-import javax.persistence.*
+import java.util.UUID
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.OneToOne
+import javax.persistence.Table
 
 @Entity
 @Table(name = "reports")
 class Report(
-        @Id
-        @GeneratedValue
-        @Column(columnDefinition = "uuid", updatable = false)
-        val uuid: UUID = UUID.randomUUID(),
+    @Id
+    @GeneratedValue
+    @Column(columnDefinition = "uuid", updatable = false)
+    val uuid: UUID = UUID.randomUUID(),
 
-        @CreatedDate
-        @Column(nullable = false)
-        val createdAt: Timestamp,
+    @CreatedDate
+    @Column(nullable = false)
+    val createdAt: Timestamp,
 
-        @Column(nullable = false)
-        val description: String,
+    @Column(nullable = false)
+    val description: String,
 
-        @Column(nullable = false)
-        val imageUrl: String,
+    @Column(nullable = false)
+    val imageUrl: String,
 
-        @OneToOne(optional = false)
-        val location: Location,
+    @OneToOne(optional = false)
+    val location: Location,
 
-        @Column(nullable = false)
-        val status: String
+    @Column(nullable = false)
+    val status: String
 )
