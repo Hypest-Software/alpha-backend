c7d68b76e1d8471a255b5c1567914d7785c3fcd8
Tomasz Domagała <tomekdo3@gmail.com>
2020-11-28 21:47:41 +0100

    add boarStatus field to ReportCreateDto

--- src/main/kotlin/dev/hypestsoftware/hackyeah2020/backend/model/dto/ReportCreateDto.kt
+++ src/main/kotlin/dev/hypestsoftware/hackyeah2020/backend/model/dto/ReportCreateDto.kt
@@ -1,17 +1,20 @@
 package dev.hypestsoftware.hackyeah2020.backend.model.dto
 
 import dev.hypestsoftware.hackyeah2020.backend.model.Report
+import dev.hypestsoftware.hackyeah2020.backend.model.ReportBoarStatus
 import dev.hypestsoftware.hackyeah2020.backend.model.ReportStatus
 
 data class ReportCreateDto(
     val description: String,
     val location: LocationDto,
-    val image: String
+    val image: String,
+    val boarStatus: ReportBoarStatus
 ) {
     fun toEntity() = Report(
         description = description,
         location = location.toEntity(),
         status = ReportStatus.NEW,
+        boarStatus = boarStatus
     )
 
 }
