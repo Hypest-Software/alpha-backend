40dca682bdc06713e219f6eba29e1857736c5aea
Tomasz Domagała <tomekdo3@gmail.com>
2020-11-28 01:39:37 +0100

    wip

--- src/main/kotlin/dev/hypestsoftware/hackyeah2020/backend/controller/SnaxController.kt
+++ src/main/kotlin/dev/hypestsoftware/hackyeah2020/backend/controller/SnaxController.kt
@@ -1,27 +1,18 @@
 package dev.hypestsoftware.hackyeah2020.backend.controller
 
-import dev.hypestsoftware.hackyeah2020.backend.exception.base.ApiErrorCode
-import dev.hypestsoftware.hackyeah2020.backend.model.User
-import dev.hypestsoftware.hackyeah2020.backend.model.dto.CurrentUserDto
-import dev.hypestsoftware.hackyeah2020.backend.service.UserService
 import dev.hypestsoftware.hackyeah2020.backend.utils.PUBLIC_API_ENDPOINT_V1
-import dev.hypestsoftware.hackyeah2020.backend.utils.TokenInfo
-import dev.hypestsoftware.hackyeah2020.backend.utils.ValidationUtils
-import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.ModelAndView
 
 @RestController
 @RequestMapping("$PUBLIC_API_ENDPOINT_V1/snax")
-class SnaxController(
-    private val userService: UserService,
-    private val tokenInfo: TokenInfo
-) {
+@PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
+class SnaxController {
     @GetMapping("/iamnot")
-    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
-    fun getInfo(): ResponseEntity<String> {
-        return ResponseEntity.ok("Przecież ja w ogóle nie mam CSa")
+    fun getInfo(): ModelAndView {
+        return ModelAndView("redirect" + "https://youtu.be/hqRNro3gif8")
     }
 }
