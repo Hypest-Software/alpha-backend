3a7fbef5729afe0dbb362e2de53ad10548ef8f1c
Tomasz Domagała <tomekdo3@gmail.com>
2020-11-28 21:45:52 +0100

    add boar status column to report table

--- src/main/resources/db/migration/V4__add_reports.sql
+++ src/main/resources/db/migration/V4__add_reports.sql
@@ -6,6 +6,7 @@ CREATE TABLE reports
     image_url   TEXT      NOT NULL,
 
     status      TEXT      NOT NULL,
+    boar_status TEXT      NOT NULL,
 
     location_id bigint references locations
 );
@@ -13,3 +14,4 @@ CREATE TABLE reports
 CREATE INDEX reports_uuid ON reports (uuid);
 CREATE INDEX reports_created_at ON reports (created_at);
 CREATE INDEX reports_status ON reports (status);
+CREATE INDEX reports_boar_status ON reports (boar_status);
