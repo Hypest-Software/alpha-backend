452960aba26d0145cadf5c7d7e9f78cc68a3ba28
Tomasz Domagała <tomekdo3@gmail.com>
2020-11-28 20:58:25 +0100

    refactor add_persisted_tokens migration

--- src/main/resources/db/migration/V4__add_reports.sql
+++ src/main/resources/db/migration/V4__add_reports.sql
@@ -1,11 +1,15 @@
 CREATE TABLE reports
 (
     uuid        uuid PRIMARY KEY,
-    created_at  TIMESTAMP     NOT NULL,
-    description TEXT          NOT NULL,
-    image_url   TEXT          NOT NULL,
+    created_at  TIMESTAMP NOT NULL,
+    description TEXT      NOT NULL,
+    image_url   TEXT      NOT NULL,
 
-    status      TEXT NOT NULL,
+    status      TEXT      NOT NULL,
 
     location_id bigint references locations
 );
+
+CREATE INDEX reports_uuid ON reports (uuid);
+CREATE INDEX reports_created_at ON reports (created_at);
+CREATE INDEX reports_status ON reports (status);
